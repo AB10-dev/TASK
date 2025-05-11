6747339e9e9f1e70a41af10bd06997e1a617bc93
AB10-dev <bansalanay10@gmail.com>
2025-05-11 19:29:55 +0530

    Add files via upload
    Assignment 1:
    Task 1 
    Task2

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+a=input('Enter the first number:')
+b=input('Enter the second number: ')
+a=int(a)
+b=int(b)
+add=a+b
+sub=a-b
+multiply=a*b
+divide=a/b
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",multiply)
+print("Division: ",divide)
